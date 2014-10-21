8591d56b22121049b8ee2f39a867419dd529b6dd
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-10-21 14:13:39 +0100

    Initial import

---
+++ lib/Acme/MetaSyntactic/legomovievideogame.pm
@@ -0,0 +1,111 @@
+package Acme::MetaSyntactic::legomovievideogame;
+
+#ABSTRACT: LEGO Movie Videogame theme
+
+use utf8;
+use strict;
+use warnings;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+
+q[Everything is awesome!];
+
+=pod
+
+=encoding utf8
+
+=head1 DESCRIPTION
+
+Characters from the LEGO Movie Videogame as gleaned from
+the L<http://www.gamefaqs.com/> walkthroughs.
+
+I<Everything is awesome!>
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Emmet
+Gail
+Frank_the_Foreman
+Demolition_Guy
+Wyldstyle
+Vitruvius_Old
+Vitruvius_Ghost
+Batman
+Bruce_Wayne
+Unikitty
+Queasy_Kitty
+Biznis_Kitty
+Astro_Kitty
+Benny
+MetalBeard
+Gandalf
+Superman
+Wonder_Woman
+Green_Lantern
+Green_Ninja
+Cleopatra
+FemBot
+Witch
+Lady_Liberty
+Abraham_Lincoln
+Shakespeare
+Michelangelo
+Magician
+Swamp_Creature
+Lord_Vampyre
+Panda_Guy
+Yeti
+Mummy
+Test_Dummy
+Caveman
+El_Macho_Wrestler
+Mrs_Scratchen_Post
+Cardio_Carrie
+Executive_Ellen
+Sharon_Shoehorn
+Where_Are_My_Pants_Guy
+Blaze_Firefighter
+Plumber_Joe
+Garbageman_Grant
+Garbageman_Dan
+Gordon_Zola
+Kabob_Bob
+Ice_Cream_Mike
+Ice_Cream_Jo
+Dr_McScrubs
+Larry_the_Barista
+Taco_Tuesday_Guy
+Gallant_Guard
+Sir_Stack_a_Brick
+Prospector
+Hank_Haystacks
+Hot_Tub_Harry
+Tomahawk
+Native
+Wiley_Fusebot
+Robo_Cowboy
+Deputron
+Calamity_Drone
+Sheriff_Not_A_Robot
+Ma_Cop
+Pa_Cop
+Velma_Staplebot
+Robo_Construction
+Robo_Demolition
+Robo_Skeleton
+Robo_SWAT_Laser
+Robo_SWAT_Rocket
+Robo_SWAT_Armour
+Robo_SWAT
+Robo_Fed
+Good_Cop
+Bad_Cop
+President_Business
+Lord_Business
